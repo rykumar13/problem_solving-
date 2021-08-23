54828672dbcfc5f833695e8df73e6d427253bd8f
Rajiv Kumar <rykumar13@gmail.com>
2021-08-23 23:41:28 +0100

    problem x

--- Sliding Window/Longest Substring with Same Letters after Replacement (hard)/solution.py
+++ Sliding Window/Longest Substring with Same Letters after Replacement (hard)/solution.py
@@ -1,18 +1,37 @@
-def max_sub_array_of_size_k(k, arr):
-    max_sum, current_sum = 0, 0
+def length_of_longest_substring(word, k):
     window_start = 0
+    char_count = {}
+    max_repeat_letter_count = 0
+    max_length = 0
 
-    for window_end in range(len(arr)):
-        current_sum += arr[window_end]
+    for window_end in range(len(word)):
 
-        if window_end >= k - 1:
-            max_sum = max(max_sum, current_sum)
-            current_sum -= arr[window_start]
+        # traverse word one character at a time and insert it into char_count dict
+        if word[window_end] not in char_count:
+            char_count[word[window_end]] = 0
+        char_count[word[window_end]] += 1
+
+        # keep track of character with the greatest count so far
+        max_repeat_letter_count = max(max_repeat_letter_count, char_count[word[window_end]])
+
+        # Current window size is from window_start to window_end, overall we have a letter which is
+        # repeating 'max_repeat_letter_count' times, this means we can have a window which has one letter
+        # repeating 'max_repeat_letter_count' times and the remaining letters we should replace.
+        # if the remaining letters are more than 'k', it is the time to shrink the window as we
+        # are not allowed to replace more than 'k' letters
+        if (window_end - window_start + 1 - max_repeat_letter_count) > k:
+            char_count[word[window_start]] -= 1
             window_start += 1
 
-    return max_sum
+        max_length = max(max_length, window_end - window_start + 1)
+
+    return max_length
 
 
-arr_in = [2, 1, 5, 1, 3, 2]
-k_in = 3
-print(max_sub_array_of_size_k(k_in, arr_in))
+def main():
+  print(length_of_longest_substring("aabccbb", 2))
+  print(length_of_longest_substring("abbcb", 1))
+  print(length_of_longest_substring("abccde", 1))
+
+
+main()
