1774e23345fbd15765af40abe8387133ea0eab83
Rajiv Kumar <rykumar13@gmail.com>
2021-08-24 17:46:58 +0100

    problem x

--- Sliding Window/Longest Subarray with Ones after Replacement (hard)/solution.py
+++ Sliding Window/Longest Subarray with Ones after Replacement (hard)/solution.py
@@ -1,29 +1,35 @@
 def length_of_longest_substring(arr, k):
-  window_start, max_length, max_ones_count = 0, 0, 0
+    window_start = 0
+    ones_count = 0
+    longest_str_so_far = 0
 
-  # Try to extend the range [window_start, window_end]
-  for window_end in range(len(arr)):
-    if arr[window_end] == 1:
-      max_ones_count += 1
+    for windows_end in range(len(arr)):
 
-    # Current window size is from window_start to window_end, overall we have a maximum of 1s
-    # repeating 'max_ones_count' times, this means we can have a window with 'max_ones_count' 1s
-    # and the remaining are 0s which should replace with 1s.
-    # now, if the remaining 0s are more than 'k', it is the time to shrink the window as we
-    # are not allowed to replace more than 'k' 0s
-    if (window_end - window_start + 1 - max_ones_count) > k:
-      if arr[window_start] == 1:
-        max_ones_count -= 1
-      window_start += 1
+        # keep track of how many 1's we have encountered so far
+        if arr[windows_end] == 1:
+            ones_count += 1
 
-    max_length = max(max_length, window_end - window_start + 1)
-  return max_length
+        # since we know how many 1's we have encountered so far
+        # the remaining elements have to be 0's
+        # check if the number of 0's are greater than k
+        while windows_end - window_start + 1 - ones_count > k:
+
+            # before shrinking window, check if outgoing element is a 1
+            # if so, update ones_count
+            if arr[window_start] == 1:
+                ones_count -= 1
+
+            # we are free to shrink the window at this point
+            window_start += 1
+
+        longest_str_so_far = max(longest_str_so_far, windows_end-window_start+1)
+
+    return longest_str_so_far
 
 
 def main():
-  print(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2))
-  print(length_of_longest_substring(
-    [0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3))
+    print(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2))
+    print(length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3))
 
 
 main()
