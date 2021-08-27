ff04f6e043cb8d75e38c0613b227e4f9257234d6
Rajiv Kumar <rykumar13@gmail.com>
2021-08-27 17:10:15 +0100

    two pointer

---
+++ Two Pointers/Pair with Target Sum (easy)/solution.py
@@ -0,0 +1,28 @@
+def pair_with_targetsum(arr, target_sum):
+    p1 = 0
+    p2 = len(arr) - 1
+
+    current_sum = arr[p1] + arr[p2]
+
+    if current_sum == target_sum:
+        return [p1, p2]
+    
+    while current_sum != target_sum:
+
+        if current_sum > target_sum:
+            p2 -= 1
+
+        if current_sum < target_sum:
+            p1 += 1
+
+        current_sum = arr[p1] + arr[p2]
+
+    return [p1, p2]
+
+
+def main():
+    print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+    print(pair_with_targetsum([2, 5, 9, 11], 11))
+
+
+main()
